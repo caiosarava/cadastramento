e930b12eb3b4598d3b1642e71cb185a5a67ad559
caiosarava <caio.sarava@gmail.com>
2026-03-11 11:26:03 -0300

    função de recuperação

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,5 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.44.4/+esm';
+
 export const SUPABASE_URL = 'https://egarccprmvgqsvivnerc.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnYXJjY3BybXZncXN2aXZuZXJjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTE2NTQ1OSwiZXhwIjoyMDc0NzQxNDU5fQ.CIZo0X0ua2teiR4zfUJnSA1WfOP_HFzDZPA0dBjsGtY';
 
@@ -10,8 +12,6 @@ export const getBaseUrl = () => {
     }
     return origin;
 };
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.44.4/+esm';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
